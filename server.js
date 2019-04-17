1dfb2e852ae8b7b8687dd3868f1f707fbccf7248
lockdown6 <38168894+lockdown6@users.noreply.github.com>
2019-04-17 11:27:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const asyncRedis = require('async-redis');
 const uuidv4 = require('uuid/v4');
 
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3001;
 
 var client = asyncRedis.createClient();
 
